9be6d562d9000ab5427d44429cf8622ad12caf73
Daniel Schwartz <danielschwar@gmail.com>
2021-10-13 20:51:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,22 @@
-terraform {
-  required_version = ">= 1.0.8"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "dansdomain"
-    workspaces {
-      name = "dansdomainnet"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
+data "aws_canonical_user_id" "current_user" {}
 
 resource "aws_s3_bucket" "bucket" {
-  bucket         = "dansdomain.net"
-  hosted_zone_id = "Z1BKCTXD74EZPE"
+  bucket = "dansdomain.net"
+
   tags = {
     "Description" = "S3 bucket for hosting dansdomain.net"
     "Name"        = "dansdomain.net"
   }
 
   grant {
-    permissions = [
-      "READ",
-      "READ_ACP",
-      "WRITE",
-    ]
-    type = "Group"
-    uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+    permissions = ["READ", "READ_ACP", "WRITE"]
+    type        = "Group"
+    uri         = "http://acs.amazonaws.com/groups/s3/LogDelivery"
   }
   grant {
-    id = "3e941c5462d8291b29f34e21d7a24877ce7b077e7c34fe52e5728329c221c281"
-    permissions = [
-      "FULL_CONTROL",
-    ]
-    type = "CanonicalUser"
+    id          = data.aws_canonical_user_id.current_user.id
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
   }
 
   logging {
@@ -57,5 +32,26 @@ resource "aws_s3_bucket" "bucket" {
   website {
     error_document = "index.html"
     index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket" "logs_bucket" {
+  bucket = "dansdomain-logs"
+  tags   = {}
+
+  grant {
+    permissions = ["READ", "READ_ACP", "WRITE"]
+    type        = "Group"
+    uri         = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+  }
+  grant {
+    id          = data.aws_canonical_user_id.current_user.id
+    permissions = ["FULL_CONTROL"]
+    type        = "CanonicalUser"
+  }
+
+  logging {
+    target_bucket = "dansdomain-logs"
+    target_prefix = "this/"
   }
 }
